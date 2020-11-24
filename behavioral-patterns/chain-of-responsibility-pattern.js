c931667dacb7e1162fc06684c290c9c2404ab48a
thangle1192 <thangle1192@gmail.com>
2020-11-24 21:49:48 +0700

    update

--- behavioral-patterns/chain-of-responsibility-pattern.js
+++ behavioral-patterns/chain-of-responsibility-pattern.js
@@ -1,23 +1,8 @@
-class Car {
-  constructor(gas) {
-    this.gas = gas;
-    this.actions = [];
-  }
-
-  setGasLevel(val) {
-    this.gas = val;
-    this.notifyAll();
-  }
-
-  register(obs) {
-    this.actions.push(obs);
-  }
-
-  unregister(obs) {
-    this.actions.filter((el) => el !== obs);
-  }
-
-  notifyAll() {
-    this.actions.forEach((el) => console.log("updated"));
-  }
-}
+/**
+ * buy ->
+ * > login handler
+ * > check address handler
+ * > taxex handler
+ * > Shipping handler
+ * > Payment processing handler
+ */
--- behavioral-patterns/state-pattern.js
+++ behavioral-patterns/state-pattern.js
@@ -1,23 +1,4 @@
-class Car {
-  constructor(gas) {
-    this.gas = gas;
-    this.actions = [];
-  }
-
-  setGasLevel(val) {
-    this.gas = val;
-    this.notifyAll();
-  }
-
-  register(obs) {
-    this.actions.push(obs);
-  }
-
-  unregister(obs) {
-    this.actions.filter((el) => el !== obs);
-  }
-
-  notifyAll() {
-    this.actions.forEach((el) => console.log("updated"));
-  }
-}
+/**
+ * like redux, state pattern store state in one place and when updated the ui is update accorrdingly
+ * to the updated state
+ */
